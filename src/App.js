e47687436f803f596b38b0f7efd98e1f09f89b76
RaviSingnath <ravisingnath@gmail.com>
2022-08-29 05:36:15 -0700

    State management with Mobx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, { createContext, useContext, useState } from 'react'
+import { useLocalStore, useObserver } from 'mobx-react'
 import './App.css';
+
+const StoreContext = createContext()
+
+const StoreProvider = ({children}) => {
+  const store = useLocalStore(() => ({
+    bugs: ['bug 1'],
+    addBug: (bug) => {
+      store.bugs.push(bug)
+    },
+    get bugCount() {
+      return store.bugs.length
+    }
+  }))
+
+  return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+}
+
+const BugsCount = () => {
+  const store = useContext(StoreContext)
+
+  return useObserver(() => (
+    <h1>Bugs: {store.bugCount}</h1>
+  ))
+}
+
+const BugList = () => {
+  const store = useContext(StoreContext)
+
+  return useObserver(() => (
+    <ul>
+      {store.bugs.map(bug => <li key={bug}>{bug}</li>)}
+    </ul>
+  ))
+}
+
+const BugForm = () => {
+  const store = useContext(StoreContext)
+  const [bug , setBug] = useState('')
+
+  return (
+    <form onSubmit={(e) => {
+      store.addBug(bug)
+      setBug('')
+      e.preventDefault()
+    }}>
+      <input type='text' value={bug} onChange={(e) => setBug(e.target.value)} />
+      <button type='submit'>Add</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <main>
+        <BugsCount />
+        <BugList />
+        <BugForm />
+      </main>
+    </StoreProvider>
   );
 }
 
