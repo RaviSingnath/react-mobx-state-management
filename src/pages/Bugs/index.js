1fa84ccfbf3ae2c6c9114b6b1532ebed4aa98d59
RaviSingnath <ravisingnath@gmail.com>
2022-09-24 18:23:51 +0530

    Added components folder

---
+++ src/pages/Bugs/index.js
@@ -0,0 +1,13 @@
+import { BugForm, BugList, BugsCount } from '../../components/Bugs'
+
+const Bugs = () => {
+  return (
+    <>
+      <BugsCount />
+      <BugList />
+      <BugForm />
+    </>
+  )
+}
+
+export default Bugs
